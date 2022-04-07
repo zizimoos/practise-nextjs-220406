08c3780354d3f5e7836ef01b8bcfa2b48e9a51fd
남형석 <azerc@MacMini.local>
2022-04-07 22:12:24 +0900

    ...test counter

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,9 @@
+import { useState } from "react";
+
 export default function Home() {
-    return "home"
+    const [count, setCount] = useState(0);
+    return (<div>
+        <h1>Home {count}</h1>
+        <button onClick={() => setCount(current => current + 1)}>Click me</button>
+    </div>)
 }
